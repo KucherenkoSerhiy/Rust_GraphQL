035119492b863cbf0c133725d98b8267ef0626bf
Serhiy <kucherenkoserhiy@gmail.com>
2016-08-20 20:17:47 +0200

    Corrected error of returning value where it was returning only single row

--- src/deserialize.rs
+++ src/deserialize.rs
@@ -1,24 +1,27 @@
+use std::option::Option;
 use mysql;
 
-pub fn perform_get(pool: &mysql::Pool, query: String, select_structure : &(&str, (&str, &str), Vec<&str>) ) -> String {
-    pool.prep_exec(query, ()).map(|mut result| {
+pub fn perform_get(pool: &mysql::Pool, query: String, select_structure : &(&str, Option<(&str, &str)>, Vec<&str>) ) -> String {
+    let mut json = "{\n  \"data\": {\n".to_string();
+    for result in pool.prep_exec(query, ()).unwrap() {
+        let mut row = result.unwrap();
         let mut resulting_object : String = "".to_string();
 
-        let mut row = result.next().unwrap().unwrap();
+        resulting_object = resulting_object + "    \"" + select_structure.0 + "\": {\n";
 
-        resulting_object = "{\n  \"data\": {\n".to_string();
         for col in &select_structure.2{
             //let data : ColumnType = row.take(*col).unwrap();
             let data : String = row.take(*col).unwrap();
             match data {
-                _ => resulting_object = resulting_object + "    \"" + col + "\": \"" + &data + "\"\n"
+                _ => resulting_object = resulting_object + "      \"" + col + "\": \"" + &data + "\"\n"
             };
         }
-        resulting_object = resulting_object + "  }\n}";
-        println!("{}", resulting_object);
+        resulting_object = resulting_object + "    }\n";
         //let name: String = row.take("name").unwrap();
-        resulting_object
+        json = json + resulting_object.as_str();
+    };
+    json = json + "  }\n}";
 
-    }).unwrap()
+    //println!("\n\n\n\n\n{}\n\n\n\n\n", json);
+    json
 }
-
